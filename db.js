ee58c22194c81a2e13faccda964f6e791bd9d490
unknown <indiandeveloper25@gmail.com>
2025-09-12 08:07:12 -0700

    fasdfA

--- db.js
+++ db.js
@@ -31,6 +31,9 @@
   }
 };
 
+
+
+
 export default connectDB();
 
   
--- server.js
+++ server.js
@@ -34,7 +34,7 @@ connectDB
 // Middlewares
 app.use(express.json())
 // app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 // app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
 // if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'));
@@ -60,7 +60,7 @@ app.use("/uploads", express.static("uploads")); // ✅ uploaded images accessibl
 
 
 // Health
-app.get('/api', (_, res) => res.json({ ok: true }));
+app.get('/api', (_, res) => res.json({ ok: true, ver: "2" }));
 
 app.get('/likeget', async (_, res) => {
 
